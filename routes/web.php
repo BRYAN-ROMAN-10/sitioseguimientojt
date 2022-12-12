893dfbe96353c9978851ba8e9f1dab5516c5759c
Bryan Roman <bryanroca2000@gmail.com>
2022-12-11 22:21:26 -0500

    v02 Incorporacion de CRUD con coneccion con base de datos MySQL

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'fnIndex']) -> name('xIndex');
+Route::post('/', [PagesController::class, 'fnRegistrar'])->name('Estudiante.xRegistrar');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/detalle/{id}', [PagesController::class, 'fnEstDetalle']) -> name('Estudiante.xDetalle');
+Route::get('/galeria/{numero?}', [PagesController::class, 'fnGaleria']) -> where('numero','[0-9]+') -> name('xGaleria');
+Route::get('/lista', [PagesController::class, 'fnLista']) -> name('xLista');
+
+Route::get('/actualizar/{id}', [PagesController::class, 'fnActualizar']) -> name('Estudiante.xActualizar');
+Route::put('/actualizar/{id}', [PagesController::class, 'fnUpdate']) -> name('Estudiante.xUpdate');
+
+Route::delete('/eliminar/{id}', [PagesController::class, 'fnEliminar']) -> name('Estudiante.xEliminar');
